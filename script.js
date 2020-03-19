aeffa81d95083d2c064df16cceca7b0ed9f27f05
Sahas Gelli <sahasgelli@Sahass-MacBook-Pro.local>
2020-03-18 21:19:05 -0500

    hello ave

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var num = 0;
 var button = document.getElementById("inc_num");
 button.onclick = show_number;
 
-var startButton = document.getElementById("exp_growth");
-startButton.onclick = exponential_growth;
+var startButton = document.getElementById("start_btn");
+// startButton.onclick = exponential_growth;
+startButton.onclick = logistic_curve;
 
 function show_number() {
     num++;
@@ -16,4 +17,14 @@ function exponential_growth() {
     // console.log("exponential growth to " + num);
     document.getElementById("display_num").innerHTML = num;
     setTimeout(exponential_growth, 1000);
+}
+
+function logistic_curve() {
+    var L = 1000000;
+    var k = .01;
+    var x;
+    var x0;
+    num = L / (1 + Math.pow(Math.E, -k * (x - x0)));
+    document.getElementById("display_num").innerHTML = num;
+    setTimeout(logistic_curve, 1000);
 }
