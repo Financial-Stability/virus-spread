6b3115203aefb2d6a4ca2c07af4731a09ccb76dc
Avery2 <JustinAveryChan@gmail.com>
2020-03-18 21:39:03 -0400

    minimal function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var num = 0;
 var button = document.getElementById("inc_num");
-button.onclick();
+button.onclick = show_number;
 
 function show_number() {
+    num++;
     document.getElementById("display_num").innerHTML = num;
 }
