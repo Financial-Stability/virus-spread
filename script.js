cdbdc2488ff98f3f5c55c192cc9d3cf76dcd8869
Avery2 <JustinAveryChan@gmail.com>
2020-03-18 21:51:08 -0400

    exponential growth

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ var num = 0;
 var button = document.getElementById("inc_num");
 button.onclick = show_number;
 
-var tit = document.getElementById("title");
-tit.onclick = show_number;
+var startButton = document.getElementById("exp_growth");
+startButton.onclick = exponential_growth;
 
 function show_number() {
     num++;
+    document.getElementById("display_num").innerHTML = num;
+}
+
+function exponential_growth() {
+    num = num * num;
+    setTimeout(exponential_growth, 1000);
+    console.log("exponential growth to " + num);
     document.getElementById("display_num").innerHTML = num;
 }
