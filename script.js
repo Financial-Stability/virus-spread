0b38a4bd6d166c542800d5493c9b797ca091b6f7
Avery2 <JustinAveryChan@gmail.com>
2020-03-18 23:36:08 -0400

    add chart library

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-var num = 0;
-
-var button = document.getElementById("inc_num");
-button.onclick = show_number;
-
-var startButton = document.getElementById("start_btn");
-// startButton.onclick = exponential_growth;
-startButton.onclick = logistic_curve;
+var ctx = document.getElementById("myChart").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              ],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
+          }
+        }
+      });
 
 function show_number() {
   num++;
@@ -29,3 +62,12 @@ function logistic_curve() {
   document.getElementById("display_num").innerHTML = num;
   setTimeout(logistic_curve, 1000);
 }
+
+var num = 0;
+
+var button = document.getElementById("inc_num");
+button.onclick = show_number;
+
+var startButton = document.getElementById("start_btn");
+// startButton.onclick = exponential_growth;
+startButton.onclick = logistic_curve;
