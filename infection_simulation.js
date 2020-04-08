1d6b0ca5755c9b7e4be769357a74f3ef5accc956
Koya <koyavsaito@gmail.com>
2020-04-07 23:40:57 -0400

    hahahus

--- infection_simulation.js
+++ infection_simulation.js
@@ -159,7 +159,7 @@ var infChart = new Chart(inf, {
         // backgroundColor: 'rgba(0,0,0,0)'
         radius: 0,
         borderWidth: 0,
-        hitRadius: 3
+        hitRadius: 20
       }
     },
     scales: {
@@ -298,7 +298,7 @@ function populate() {
   // add walls
   let wall_length = Math.floor(side_size * 0.1);
   let walls_per_side = Math.floor(side_size * 0.1);
-
+  let dist_btw_walls = Math.floor()
   
   for(var i=0; i<4; i++){
     //for each side
