436a322c04680cc67e24d22ac4c085cebcb0778d
Koya <koyavsaito@gmail.com>
2020-04-07 23:26:21 -0400

    hahahus

--- infection_simulation.js
+++ infection_simulation.js
@@ -164,7 +164,14 @@ var infChart = new Chart(inf, {
           display: false
         }
       ]
-    }
+    },
+    animation: {
+      duration: 0 // general animation time
+  },
+  hover: {
+      animationDuration: 0 // duration of animations when hovering an item
+  },
+  responsiveAnimationDuration: 0 // animation duration after a resize
   }
 });
 
